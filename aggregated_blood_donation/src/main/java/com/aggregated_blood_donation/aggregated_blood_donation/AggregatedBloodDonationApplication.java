18f721ac5a138e74dbcc8d24c10b84f993293802
Hermitcrabb <prathambhandari2000@gmail.com>
2025-09-26 10:29:55 +0545

    start of project

---
+++ aggregated_blood_donation/src/main/java/com/aggregated_blood_donation/aggregated_blood_donation/AggregatedBloodDonationApplication.java
@@ -0,0 +1,13 @@
+package com.aggregated_blood_donation.aggregated_blood_donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatedBloodDonationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatedBloodDonationApplication.class, args);
+    }
+
+}
---
+++ aggregated_blood_donation/src/test/java/com/aggregated_blood_donation/aggregated_blood_donation/AggregatedBloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggregated_blood_donation.aggregated_blood_donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatedBloodDonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
